1276edbef9517e2a38ecb1bd744545e7670faa91
Dale Fugier <dale@mcneel.com>
2023-09-26 12:48:19 -0700

    Updated to SDK-style projects. Still targeting .NET48

--- Vectorize/Properties/AssemblyInfo.cs
+++ Vectorize/Properties/AssemblyInfo.cs
@@ -1,30 +1,14 @@
 ﻿using Rhino.PlugIns;
-using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: PlugInDescription(DescriptionType.Address, "Seattle, WA")]
+[assembly: PlugInDescription(DescriptionType.Address, "146 North Canal Street, Suite 320\r\nSeattle, WA 98103")]
 [assembly: PlugInDescription(DescriptionType.Country, "United States")]
 [assembly: PlugInDescription(DescriptionType.Email, "dale@mcneel.com")]
-[assembly: PlugInDescription(DescriptionType.Phone, "-")]
-[assembly: PlugInDescription(DescriptionType.Fax, "-")]
-[assembly: PlugInDescription(DescriptionType.Organization, "Dale Fugier")]
-[assembly: PlugInDescription(DescriptionType.UpdateUrl, "https://www.food4rhino.com/app/vectorize")]
-[assembly: PlugInDescription(DescriptionType.WebSite, "https://github.com/dalefugier")]
+[assembly: PlugInDescription(DescriptionType.Phone, "206-545-6877")]
+[assembly: PlugInDescription(DescriptionType.Fax, "206-545-7321")]
+[assembly: PlugInDescription(DescriptionType.Organization, "Robert McNeel & Associates")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "https://www.food4rhino.com/en/app/vectorize")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "https://www.food4rhino.com/en/app/vectorize")]
 [assembly: PlugInDescription(DescriptionType.Icon, "Vectorize.Resources.Vectorize.ico")]
-
-[assembly: AssemblyTitle("Vectorize")]
-[assembly: AssemblyDescription("Vectorize plug-in for Rhino")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dale Fugier")]
-[assembly: AssemblyProduct("Vectorize")]
-[assembly: AssemblyCopyright("Copyright (C) 2020-2023, Dale Fugier")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4c63f2be-7c08-4c8b-8186-a24d29c69bcd")]
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.0.*")]
-//[assembly: AssemblyFileVersion("8.0.0.0")]
-[assembly: AssemblyInformationalVersion("2")]
