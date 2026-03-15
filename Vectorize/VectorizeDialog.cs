77b2bd6b99f749a6842cded53a1ae7788b83b0f4
Dale <dale@mcneel.com>
2026-03-15 09:25:55 -0700

    Dialog and NuGet updates

--- Vectorize/VectorizeDialog.cs
+++ Vectorize/VectorizeDialog.cs
@@ -4,6 +4,7 @@ using Rhino;
 using Rhino.Runtime;
 using Rhino.UI.Controls;
 using Rhino.UI.Forms;
+using System.ComponentModel;
 using VectorizeCommon;
 
 namespace Vectorize
@@ -15,7 +16,6 @@ namespace Vectorize
   {
     private readonly RhinoDoc m_doc;
     private readonly VectorizeConduit m_conduit;
-    private bool m_update_and_redraw = true;
 
     /// <summary>
     /// Public constructor
@@ -31,7 +31,6 @@ namespace Vectorize
 
       Title = VectorizeCommand.Instance.EnglishName;
       Content = CreateTableLayout();
-      m_update_and_redraw = false;
       Shown += (sender, e) => UpdateAndRedraw();
     }
 
@@ -40,129 +39,91 @@ namespace Vectorize
     /// </summary>
     private RhinoDialogTableLayout CreateTableLayout()
     {
-      PotraceParameters parameters = m_conduit.Parameters;
+      RhinoDialogTableLayout layout = new RhinoDialogTableLayout(false);
 
-      // Threshold (0.0, 100.0)
-      Rhino.UI.Controls.Slider sldThreshold = new Rhino.UI.Controls.Slider(this, true)
+      // Threshold slider (0.0, 100.0)
+      Rhino.UI.Controls.Slider thresholdSlider = new Rhino.UI.Controls.Slider(layout, true)
       {
         Decimals = 2,
         DrawTextLabels = true,
         ToolTip = PotraceStrings.ThresholdTooltip(false),
-        Value1 = parameters.ThresholdUi,
+        Value1 = m_conduit.Parameters.ThresholdUi,
         Width = 200
       };
-      sldThreshold.SetMinMax(0.0, 100.0);
-      sldThreshold.MouseUp += (sender, args) =>
-      {
-        double value = sldThreshold.Value1.Value;
-        if (parameters.ThresholdUi != value)
-        {
-          parameters.ThresholdUi = value;
-          UpdateAndRedraw();
-        }
-      };
+      thresholdSlider.SetMinMax(0.0, 100.0);
+      thresholdSlider.PropertyChanged += OnThresholdPropertyChanged;
 
-      // TurdSize (0 to 100)
-      Rhino.UI.Controls.Slider sldTurdSize = new Rhino.UI.Controls.Slider(this, false)
+      // TurdSize slider (0 to 100)
+      Rhino.UI.Controls.Slider turdSizeSlider = new Rhino.UI.Controls.Slider(this, false)
       {
         Decimals = 0,
         DrawTextLabels = true,
         ToolTip = PotraceStrings.TurdSizeTooltip(false),
-        Value1 = parameters.TurdSize
+        Value1 = m_conduit.Parameters.TurdSize
       };
-      sldTurdSize.SetMinMax(0.0, 100.0);
-      sldTurdSize.MouseUp += (sender, args) =>
-      {
-        int value = (int)sldTurdSize.Value1.Value;
-        if (parameters.TurdSize != value)
-        {
-          parameters.TurdSize = value;
-          UpdateAndRedraw();
-        }
-      };
+      turdSizeSlider.SetMinMax(0.0, 100.0);
+      turdSizeSlider.PropertyChanged += OnTurdSizePropertyChanged;
 
-      // AlphaMax (0.0, 1.34)
-      Rhino.UI.Controls.Slider sldAlphaMax = new Rhino.UI.Controls.Slider(this, false)
+      // AlphaMax slider (0.0, 1.34)
+      Rhino.UI.Controls.Slider alphaMaxSlider = new Rhino.UI.Controls.Slider(this, false)
       {
         Decimals = 2,
         DrawTextLabels = true,
         ToolTip = PotraceStrings.AlphaMaxTooltip(false),
-        Value1 = parameters.AlphaMax
+        Value1 = m_conduit.Parameters.AlphaMax
       };
-      sldAlphaMax.SetMinMax(0.0, 1.34);
-      sldAlphaMax.MouseUp += (sender, args) =>
-      {
-        double value = sldAlphaMax.Value1.Value;
-        if (parameters.AlphaMax != value)
-        {
-          parameters.AlphaMax = value;
-          UpdateAndRedraw();
-        }
-      };
+      alphaMaxSlider.SetMinMax(0.0, 1.34);
+      alphaMaxSlider.PropertyChanged += OnAlphaMaxPropertyChanged;
 
-      // OptimizeTolerance (0.0, 1.0)
-      Rhino.UI.Controls.Slider sldOptimizeTolerance = new Rhino.UI.Controls.Slider(this, false)
+      // OptimizeTolerance slider (0.0, 1.0)
+      Rhino.UI.Controls.Slider optimizeToleranceSlider = new Rhino.UI.Controls.Slider(this, false)
       {
         Decimals = 2,
         DrawTextLabels = true,
         ToolTip = PotraceStrings.OptimizeToleranceTooltip(false),
-        Value1 = parameters.OptimizeTolerance
+        Value1 = m_conduit.Parameters.OptimizeTolerance
       };
-      sldOptimizeTolerance.SetMinMax(0.0, 1.0);
-      sldOptimizeTolerance.MouseUp += (sender, args) =>
-      {
-        double value = sldOptimizeTolerance.Value1.Value;
-        if (parameters.OptimizeTolerance != value)
-        {
-          parameters.OptimizeTolerance = value;
-          UpdateAndRedraw();
-        }
-      };
+      optimizeToleranceSlider.SetMinMax(0.0, 1.0);
+      optimizeToleranceSlider.PropertyChanged += OnOptimizeTolerancePropertyChanged;
 
-      // IncludeBorder
-      CheckBox chkIncludeBorder = new CheckBox
+      // IncludeBorder checkbox
+      CheckBox includeBorderCheckBox = new CheckBox
       {
-        Checked = parameters.IncludeBorder,
+        Checked = m_conduit.Parameters.IncludeBorder,
         ThreeState = false,
         ToolTip = PotraceStrings.IncludeBorderTooltip
       };
-      chkIncludeBorder.CheckedChanged += (sender, args) =>
+      includeBorderCheckBox.CheckedChanged += (sender, args) =>
       {
-        bool value = chkIncludeBorder.Checked.Value;
-        if (parameters.IncludeBorder != value)
+        bool value = includeBorderCheckBox.Checked.Value;
+        if (m_conduit.Parameters.IncludeBorder != value)
         {
-          parameters.IncludeBorder = value;
+          m_conduit.Parameters.IncludeBorder = value;
           m_doc.Views.Redraw();
         }
       };
 
-      // RestoreDefaults
-      Button btnRestoreDefaults = new Button { Text = "Restore Defaults" };
-      btnRestoreDefaults.Click += (sender, args) =>
+      // RestoreDefaults button
+      Button restoreDefaultsButton = new Button { Text = "Restore Defaults" };
+      restoreDefaultsButton.Click += (sender, args) =>
       {
-        m_update_and_redraw = true;
-        parameters.SetDefaults();
-        sldThreshold.Value1 = parameters.ThresholdUi;
-        sldTurdSize.Value1 = parameters.TurdSize;
-        sldAlphaMax.Value1 = parameters.AlphaMax;
-        sldOptimizeTolerance.Value1 = parameters.OptimizeTolerance;
-        chkIncludeBorder.Checked = parameters.IncludeBorder;
-        m_update_and_redraw = false;
+        m_conduit.Parameters.SetDefaults();
+        thresholdSlider.Value1 = m_conduit.Parameters.ThresholdUi;
+        turdSizeSlider.Value1 = m_conduit.Parameters.TurdSize;
+        alphaMaxSlider.Value1 = m_conduit.Parameters.AlphaMax;
+        optimizeToleranceSlider.Value1 = m_conduit.Parameters.OptimizeTolerance;
+        includeBorderCheckBox.Checked = m_conduit.Parameters.IncludeBorder;
         UpdateAndRedraw();
       };
 
       // Layout the controls
-
-      RhinoDialogTableLayout layout = new RhinoDialogTableLayout(false); // { Spacing = new Eto.Drawing.Size(10, 8) };
-      //layout.Rows.Add(new TableRow(new TableCell(new LabelSeparator { Text = "Vectorization options" }, true)));
-
       TableLayout table = new TableLayout { Padding = new Eto.Drawing.Padding(8, 0, 0, 0), Spacing = new Size(10, 8) };
-      table.Rows.Add(new TableRow(new TableCell(new Label { Text = PotraceStrings.ThresholdLabel(false) }), new TableCell(sldThreshold)));
-      table.Rows.Add(new TableRow(new TableCell(new Label { Text = PotraceStrings.TurdSizeLabel(true) }), new TableCell(sldTurdSize)));
-      table.Rows.Add(new TableRow(new TableCell(new Label { Text = PotraceStrings.AlphaMaxLabel(true) }), new TableCell(sldAlphaMax)));
-      table.Rows.Add(new TableRow(new TableCell(new Label { Text = PotraceStrings.OptimizeToleranceLabel(false) }), new TableCell(sldOptimizeTolerance)));
-      table.Rows.Add(new TableRow(new TableCell(new Label { Text = PotraceStrings.IncludeBorderLabel(true) }), new TableCell(chkIncludeBorder)));
-      table.Rows.Add(new TableRow(new TableCell(new Label { Text = "" }), new TableCell(btnRestoreDefaults)));
+      table.Rows.Add(new TableRow(new TableCell(new Label { Text = PotraceStrings.ThresholdLabel(false) }), new TableCell(thresholdSlider)));
+      table.Rows.Add(new TableRow(new TableCell(new Label { Text = PotraceStrings.TurdSizeLabel(true) }), new TableCell(turdSizeSlider)));
+      table.Rows.Add(new TableRow(new TableCell(new Label { Text = PotraceStrings.AlphaMaxLabel(true) }), new TableCell(alphaMaxSlider)));
+      table.Rows.Add(new TableRow(new TableCell(new Label { Text = PotraceStrings.OptimizeToleranceLabel(false) }), new TableCell(optimizeToleranceSlider)));
+      table.Rows.Add(new TableRow(new TableCell(new Label { Text = PotraceStrings.IncludeBorderLabel(true) }), new TableCell(includeBorderCheckBox)));
+      table.Rows.Add(new TableRow(new TableCell(new Label { Text = "" }), new TableCell(restoreDefaultsButton)));
       table.Rows.Add(null);
 
       layout.Rows.Add(table);
@@ -172,22 +133,82 @@ namespace Vectorize
     }
 
     /// <summary>
+    /// Threshold slider handler
+    /// </summary>
+    private void OnThresholdPropertyChanged(object sender, PropertyChangedEventArgs args)
+    {
+      if (sender is Rhino.UI.Controls.Slider slider && args.PropertyName.Equals("OnEndChanged"))
+      {
+        double value = (double)slider.Value1;
+        if (m_conduit.Parameters.ThresholdUi != value)
+        {
+          m_conduit.Parameters.ThresholdUi = value;
+          UpdateAndRedraw();
+        }
+      }
+    }
+
+    /// <summary>
+    /// TurdSize slider handler
+    /// </summary>
+    private void OnTurdSizePropertyChanged(object sender, PropertyChangedEventArgs args)
+    {
+      if (sender is Rhino.UI.Controls.Slider slider && args.PropertyName.Equals("OnEndChanged"))
+      {
+        int value = (int)slider.Value1;
+        if (m_conduit.Parameters.TurdSize != value)
+        {
+          m_conduit.Parameters.TurdSize = value;
+          UpdateAndRedraw();
+        }
+      }
+    }
+
+    /// <summary>
+    /// AlphaMax slider handler
+    /// </summary>
+    private void OnAlphaMaxPropertyChanged(object sender, PropertyChangedEventArgs args)
+    {
+      if (sender is Rhino.UI.Controls.Slider slider && args.PropertyName.Equals("OnEndChanged"))
+      {
+        double value = (double)slider.Value1;
+        if (m_conduit.Parameters.AlphaMax != value)
+        {
+          m_conduit.Parameters.AlphaMax = value;
+          UpdateAndRedraw();
+        }
+      }
+    }
+
+    /// <summary>
+    /// OptimizeTolerance slider handler
+    /// </summary>
+    private void OnOptimizeTolerancePropertyChanged(object sender, PropertyChangedEventArgs args)
+    {
+      if (sender is Rhino.UI.Controls.Slider slider && args.PropertyName.Equals("OnEndChanged"))
+      {
+        double value = (double)slider.Value1;
+        if (m_conduit.Parameters.OptimizeTolerance != value)
+        {
+          m_conduit.Parameters.OptimizeTolerance = value;
+          UpdateAndRedraw();
+        }
+      }
+    }
+    
+    /// <summary>
     /// Trace the bitmap and redraw the results.
     /// </summary>
     private void UpdateAndRedraw()
     {
-      if (m_update_and_redraw || null == m_doc || null == m_conduit)
+      if (null == m_doc || null == m_conduit)
         return;
-
-      m_update_and_redraw = true;
 
       RhinoApp.SetCommandPrompt("Tracing image, please wait");
       m_conduit.TraceBitmap();
       m_doc.Views.Redraw();
       string msg = HostUtils.RunningOnOSX ? "Apply" : "OK";
       RhinoApp.SetCommandPrompt($"Vectorize options. Press {msg} when done");
-
-      m_update_and_redraw = false;
     }
 
     /// <summary>
